507b071a32c2774748e876707b89cf49ccd6ffdb
DeonVosloo <deonvosloo3366@gmail.com>
2023-06-20 16:10:13 +0200

    cleanup

--- multipcon/src/pages/LoginPage.js
+++ multipcon/src/pages/LoginPage.js
@@ -46,7 +46,7 @@ export default function LoginPage() {
   return (
     <>
       <Helmet>
-        <title> Login | Minimal UI </title>
+        <title> Login | Multi paint & construction </title>
       </Helmet>
 
       <StyledRoot>
