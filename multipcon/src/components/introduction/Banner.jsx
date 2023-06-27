490944a7357f8715828a579750eb73773db59623
DeonVosloo <deonvosloo3366@gmail.com>
2023-06-27 12:11:27 +0200

    changedL appbar, background, icon, banner

--- multipcon/src/components/introduction/Banner.jsx
+++ multipcon/src/components/introduction/Banner.jsx
@@ -3,22 +3,25 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
-import BannerImage from '../../images/banner.jpg'
+import BannerImagePNG from '../../images/banner.png';
+// import BannerImageSVG from '../../images/painting.svg';
 
 function Banner() {
   return (
     <Container maxWidth="1500" sx={{backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center",
-    backgroundImage: `url(${BannerImage})`, height: 250, p:"2%" }} >
+    backgroundImage: `url(${BannerImagePNG})`, height: {xl: 450, xs: 250, md:300, lg: 350 }}} >
         <Box component="div">
     <Grid>
       <Grid item md={6}>
-        <Typography component="h1" variant="h5" color="white" gutterBottom sx={{textAlign: "start", paddingLeft: "4%", paddingTop: "4%", fontFamily: `Ubuntu, sans-serif`}}>
-          Hello I'm, Deon Vosloo a Fullstack Software Developer.
+        <Typography component="h1" variant="h5" color="white" gutterBottom sx={{textAlign: "start", paddingLeft: "4%", paddingTop: "4%", fontFamily: `Ubuntu, sans-serif`, 
+        fontSize: {xl: 50, md: 40, xs: 24} , color: 'black'}}>
+          Multi Paint & Construction
         </Typography>
       </Grid>
       <Grid item md={6}>
-        <Typography component="h1" variant="h5" color="white" gutterBottom sx={{textAlign: "center", paddingTop: "4%", fontFamily: `Ubuntu, sans-serif`}}>
-         Welcome to my Portfolio
+        <Typography component="h1" variant="h5" color="white" gutterBottom sx={{textAlign: "center", paddingTop: "4%", fontFamily: `Ubuntu, sans-serif`,
+        fontSize: {xl: 50, md: 40, xs: 24}, color: 'black'}}>
+         Welcome to our website
         </Typography>
       </Grid>
     </Grid>
