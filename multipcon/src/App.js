f9deccd9e30642b88a4206576ee9b54bf9a8dc81
DeonVosloo <deonvosloo3366@gmail.com>
2023-06-29 14:40:52 +0200

    Fixing errors

--- multipcon/src/App.js
+++ multipcon/src/App.js
@@ -3,6 +3,8 @@ import AppHeader from './components/header/Appheader'
 import Introduction from './components/introduction/Introduction';
 import ExperiencesGrid from './components/experiences/Experiencesgrid';
 import Footer from './components/footer/Footer';
+import ConstructionIcon from '@mui/icons-material/Construction';
+import Typography from '@mui/material/Typography'
 
 function App() {
   return (
@@ -12,7 +14,7 @@ function App() {
       <div id="About">
         <Introduction/>
       </div>
-      
+      <ConstructionIcon sx={{fontSize: 72}}/><Typography variant="h1" color="initial">This website is still under construction and is not near completed </Typography><ConstructionIcon sx={{fontSize: 72}}/>
       <div id="Work">
         <ExperiencesGrid/>
       </div>
