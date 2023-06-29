8334ee45a0c66d9ee42dee77d9ab042f9f2d3bf3
DeonVosloo <deonvosloo3366@gmail.com>
2023-06-29 11:14:00 +0200

    banner improvements on scalability

--- multipcon/src/components/introduction/Banner.jsx
+++ multipcon/src/components/introduction/Banner.jsx
@@ -9,18 +9,19 @@ import BannerImageSVG from '../../images/banner.svg';
 function Banner() {
   return (
       <Container maxWidth="5500" sx={{backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center",
-    backgroundImage: `url(${BannerImagePNG})`, height: {xl: 450, xs: 250, md:300, lg: 350 }, border: 'solid', borderRadius: 0.2, borderColor: 'grey.600' }} >
+    backgroundImage: `url(${BannerImageSVG})`, height: {xl: '130%', xs: '130%', md:'130%', lg: '130%' }, border: {lg: 2, sm:0, md:1.5},
+     borderRadius: {lg:3, xs:0}, borderColor: {lg:'grey.600'} }} >
         <Box component="div">
     <Grid>
       <Grid item md={12}>
         <Typography component="h1" variant="h5" color="white" gutterBottom sx={{textAlign: "start", paddingLeft: "4%", paddingTop: "4%", fontFamily: `Ubuntu, sans-serif`, 
-        fontSize: {xl: 50, md: 40, xs: 24} , color: 'black'}}>
+        fontSize: {xl: 80, md: 55, xs: 32} , color: 'black'}}>
           Multi Paint & Construction
         </Typography>
       </Grid>
-      <Grid item md={8}>
+      <Grid item md={9} xs={7}>
         <Typography component="h1" variant="h5" color="white" gutterBottom sx={{textAlign: "center", paddingTop: "4%", fontFamily: `Ubuntu, sans-serif`,
-        fontSize: {xl: 50, md: 40, xs: 24}, color: 'black'}}>
+        fontSize: {xl: 55, md: 40, xs: 24}, color: 'black'}}>
          Welcome to our website
         </Typography>
       </Grid>
