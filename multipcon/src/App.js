9890e293467590bc21b76390f522916d132b45cc
DeonVosloo <deonvosloo3366@gmail.com>
2023-06-29 16:39:53 +0200

    fixed under construction message

--- multipcon/src/App.js
+++ multipcon/src/App.js
@@ -5,6 +5,7 @@ import ExperiencesGrid from './components/experiences/Experiencesgrid';
 import Footer from './components/footer/Footer';
 import ConstructionIcon from '@mui/icons-material/Construction';
 import Typography from '@mui/material/Typography'
+import Grid from '@mui/material/Grid'
 
 function App() {
   return (
@@ -17,7 +18,10 @@ function App() {
       <div id="Work">
         <ExperiencesGrid/>
       </div>
+      <Grid lg={11.5} md={11.5} xs={11.5} >
       <ConstructionIcon sx={{fontSize: 72}}/><Typography variant="h1" color="initial">This website is still under construction</Typography><ConstructionIcon sx={{fontSize: 72}}/>
+      </Grid>
+      
 
       <Footer/>
       
