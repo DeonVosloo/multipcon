634dc63bbd859a927070418e00b233ef80a5aacc
DeonVosloo <deonvosloo3366@gmail.com>
2023-07-03 10:29:06 +0200

    Added under construction message in home page

--- multipcon/src/pages/Home.jsx
+++ multipcon/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import Introduction from '../components/introduction/Introduction';
 import Experiences from '../components/experiences/Experiences';
 import Appheader from '../components/header/Appheader';
 import Footer from '../components/footer/Footer';
+import ConstructionIcon from '@mui/icons-material/Construction';
+import { Grid, Typography } from '@mui/material';
 
 function Home() {
     return (
@@ -15,6 +17,7 @@ function Home() {
       <div id="Work">
         <Experiences/>
       </div>
+      <Grid xl={12}><Typography><ConstructionIcon sx={{fontSize: 40}}/> This site is under construction <ConstructionIcon sx={{fontSize: 40}}/></Typography></Grid>
 
       </>
      
