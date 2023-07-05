125c582942eecde45863efd19fcf26da1cb9876a
DeonVosloo <deonvosloo3366@gmail.com>
2023-07-05 15:06:47 +0200

    About us page, specialisations added

--- multipcon/src/components/about/WorkWeDo.jsx
+++ multipcon/src/components/about/WorkWeDo.jsx
@@ -2,6 +2,12 @@ import { Box, Grid } from '@mui/material'
 import React from 'react'
 import Waterproofing from './workWeDo/Waterproofing'
 import DampProofing from './workWeDo/DampProofing'
+import Painting from './workWeDo/Painting'
+import RoofPainting from './workWeDo/RoofPainting'
+import Tiling from './workWeDo/Tiling'
+import BathroomRenovations from './workWeDo/BathroomRenovations'
+import PoolAndPumpRepairs from './workWeDo/PoolAndPumpRepairs'
+import Paving from './workWeDo/Paving'
 
 export default function WorkWeDo() {
   return (
@@ -13,6 +19,24 @@ export default function WorkWeDo() {
             </Grid>
             <Grid item xs={12} sm={12} md={12} lg={12} >
               <DampProofing/>
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12} >
+              <Painting/>
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12} >
+              <RoofPainting/>
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12} >
+              <Tiling/>
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12} >
+              <BathroomRenovations/>
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12} >
+              <PoolAndPumpRepairs/>
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12} >
+              <Paving/>
             </Grid>
         </Grid>
     </Box>
